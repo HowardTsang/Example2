18e0405cf8f4cf9561da385d5e58613e990ac085
HowardTsang <hotsang@deloitte.ie>
2021-03-06 15:07:09 +0000

    Added module2.py

--- Module3.py
+++ Module3.py
@@ -503,7 +503,10 @@ print(cars.loc[:, ['cars_per_cap', 'drives_right']])
 
 
 
-
+#In the editor on the right, write code to see if True equals False.
+#Write Python code to check if -5 * 15 is not equal to 75.
+#Ask Python whether the strings "pyscript" and "PyScript" are equal.
+#What happens if you compare booleans and integers? Write code to see if True and 1 are equal.
 
 
 
--- Test.py
+++ Test.py
@@ -1,1 +1,1 @@
-Print("Hello")
+import numpy as np
