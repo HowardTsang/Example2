95df5ab27507b87a8707d5b15e2c34efcaf0e3e3
HowardTsang <hotsang@deloitte.ie>
2021-03-17 12:19:56 +0000

    Added Module 8.py

---
+++ Module 8.py
@@ -0,0 +1,1171 @@
+#Import Matplotlib and Seaborn using the standard naming convention.
+# Import Matplotlib and Seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+
+#Create a scatter plot of GDP (gdp) vs. number of phones per 1000 people (phones).
+# Import Matplotlib and Seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Create scatter plot with GDP on the x-axis and number of phones on the y-axis
+sns.scatterplot(x=gdp, y=phones)
+
+
+
+
+#Display the plot.
+# Import Matplotlib and Seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Create scatter plot with GDP on the x-axis and number of phones on the y-axis
+sns.scatterplot(x=gdp, y=phones)
+
+# Show plot
+plt.show()
+
+
+
+
+#Change the scatter plot so it displays the percent of the population that can read and write (percent_literate) on the y-axis.
+# Import Matplotlib and Seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Change this scatter plot to have percent literate on the y-axis
+sns.scatterplot(x=gdp, y=percent_literate)
+
+# Show plot
+plt.show()
+
+
+
+#Import Matplotlib and Seaborn using the standard naming conventions.
+#Use Seaborn to create a count plot with region on the y-axis.
+#Display the plot.
+# Import Matplotlib and Seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Create count plot with region on the y-axis
+sns.countplot(y=region)
+
+# Show plot
+plt.show()
+
+
+
+
+#Read the csv file located at csv_filepath into a DataFrame named df.
+#Print the head of df to show the first five rows.
+# Import pandas
+import pandas as pd
+
+# Create a DataFrame from csv file
+df = pd.read_csv(csv_filepath)
+
+# Print the head of df
+print(df.head())
+
+
+
+
+#Import Matplotlib, Pandas, and Seaborn using the standard names.
+#Create a DataFrame named df from the csv file located at csv_filepath.
+#Use the countplot() function with the x= and data= arguments to create a count plot with the "Spiders" column values on the x-axis.
+#Display the plot.
+# Import Matplotlib, Pandas, and Seaborn
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+# Create a DataFrame from csv file
+df = pd.read_csv(csv_filepath)
+
+# Create a count plot with "Spiders" on the x-axis
+sns.countplot(x="Spiders", data=df)
+
+# Display the plot
+plt.show()
+
+
+
+
+#Create a scatter plot with "absences" on the x-axis and final grade ("G3") on the y-axis using the DataFrame student_data. Color the plot points based on "location" (urban vs. rural).
+# Import Matplotlib and Seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Create a scatter plot of absences vs. final grade
+sns.scatterplot(x="absences", y="G3",
+                data=student_data,
+                hue="location")
+
+# Show plot
+plt.show()
+
+
+
+
+#Make "Rural" appear before "Urban" in the plot legend.
+# Import Matplotlib and Seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Change the legend order in the scatter plot
+sns.scatterplot(x="absences", y="G3",
+                data=student_data,
+                hue="location",
+                hue_order=["Rural", "Urban"])
+
+# Show plot
+plt.show()
+
+
+
+
+
+#Fill in the palette_colors dictionary to map the "Rural" location value to the color "green" and the "Urban" location value to the color "blue".
+#Create a count plot with "school" on the x-axis using the student_data DataFrame.
+#Add subgroups to the plot using "location" variable and use the palette_colors dictionary to make the location subgroups green and blue.
+# Import Matplotlib and Seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Create a dictionary mapping subgroup values to colors
+palette_colors = {"Rural": "green", "Urban": "blue"}
+
+# Create a count plot of school with location subgroups
+sns.countplot(x="school", data=student_data,
+              hue="location",
+              palette=palette_colors)
+
+# Display plot
+plt.show()
+
+
+
+
+
+#Modify the code to use relplot() instead of scatterplot().
+# Change to use relplot() instead of scatterplot()
+sns.relplot(x="absences", y="G3",
+            data=student_data,
+            kind="scatter")
+
+# Show plot
+plt.show()
+
+
+
+
+#Modify the code to create one scatter plot for each level of the variable "study_time", arranged in columns.
+# Change to make subplots based on study time
+sns.relplot(x="absences", y="G3",
+            data=student_data,
+            kind="scatter",
+            col="study_time")
+
+# Show plot
+plt.show()
+
+
+
+#Adapt your code to create one scatter plot for each level of a student's weekly study time, this time arranged in rows.
+# Change this scatter plot to arrange the plots in rows instead of columns
+sns.relplot(x="absences", y="G3",
+            data=student_data,
+            kind="scatter",
+            row="study_time")
+
+# Show plot
+plt.show()
+
+
+
+
+#Use relplot() to create a scatter plot with "G1" on the x-axis and "G3" on the y-axis, using the student_data DataFrame.
+# Create a scatter plot of G1 vs. G3
+sns.relplot(x="G1", y="G3",
+            data=student_data,
+            kind="scatter")
+
+# Show plot
+plt.show()
+
+
+
+#Create column subplots based on whether the student received support from the school ("schoolsup"), ordered so that "yes" comes before "no".
+# Adjust to add subplots based on school support
+sns.relplot(x="G1", y="G3",
+            data=student_data,
+            kind="scatter",
+            col="schoolsup",
+            col_order=["yes", "no"])
+
+# Show plot
+plt.show()
+
+
+
+
+#Add row subplots based on whether the student received support from the family ("famsup"), ordered so that "yes" comes before "no". This will result in subplots based on two factors.
+# Adjust further to add subplots based on family support
+sns.relplot(x="G1", y="G3",
+            data=student_data,
+            kind="scatter",
+            col="schoolsup",
+            col_order=["yes", "no"],
+            row="famsup",
+            row_order=["yes", "no"])
+
+# Show plot
+plt.show()
+
+
+
+
+#Use relplot() and the mpg DataFrame to create a scatter plot with "horsepower" on the x-axis and "mpg" on the y-axis. Vary the size of the points by the number of cylinders in the car ("cylinders").
+# Import Matplotlib and Seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Create scatter plot of horsepower vs. mpg
+sns.relplot(x="horsepower", y="mpg",
+            data=mpg, kind="scatter",
+            size="cylinders")
+
+# Show plot
+plt.show()
+
+
+
+#To make this plot easier to read, use hue to vary the color of the points by the number of cylinders in the car ("cylinders").
+# Import Matplotlib and Seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Create scatter plot of horsepower vs. mpg
+sns.relplot(x="horsepower", y="mpg",
+            data=mpg, kind="scatter",
+            size="cylinders", hue="cylinders")
+
+# Show plot
+plt.show()
+
+
+
+#Use relplot() and the mpg DataFrame to create a scatter plot with "acceleration" on the x-axis and "mpg" on the y-axis. Vary the style and color of the plot points by country of origin ("origin").
+# Import Matplotlib and Seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Create a scatter plot of acceleration vs. mpg
+sns.relplot(x="acceleration", y="mpg",
+            data=mpg, kind="scatter",
+            style="origin", hue="origin")
+
+# Show plot
+plt.show()
+
+
+
+#Use relplot() and the mpg DataFrame to create a line plot with "model_year" on the x-axis and "mpg" on the y-axis.
+# Import Matplotlib and Seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Create line plot
+sns.relplot(x="model_year", y="mpg",
+            data=mpg, kind="line")
+
+# Show plot
+plt.show()
+
+
+
+
+#Use relplot() and the mpg DataFrame to create a line plot with "model_year" on the x-axis and "horsepower" on the y-axis. Turn off the confidence intervals on the plot.
+# Import Matplotlib and Seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Create line plot of model year vs. horsepower
+sns.relplot(x="model_year", y="horsepower",
+            data=mpg, kind="line",
+            ci=None)
+
+# Show plot
+plt.show()
+
+
+
+
+#Create different lines for each country of origin ("origin") that vary in both line style and color.
+# Import Matplotlib and Seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Change to create subgroups for country of origin
+sns.relplot(x="model_year", y="horsepower",
+            data=mpg, kind="line",
+            ci=None, style="origin",
+            hue="origin")
+
+# Show plot
+plt.show()
+
+
+
+
+#Add markers for each data point to the lines.
+#Use the dashes parameter to use solid lines for all countries, while still allowing for different marker styles for each line.
+# Import Matplotlib and Seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Add markers and make each line have the same style
+sns.relplot(x="model_year", y="horsepower",
+            data=mpg, kind="line",
+            ci=None, style="origin",
+            hue="origin", markers=True,
+            dashes=False)
+
+# Show plot
+plt.show()
+
+
+
+#Change the plot so the shaded area shows the standard deviation instead of the confidence interval for the mean.
+# Make the shaded area show the standard deviation
+sns.relplot(x="model_year", y="mpg",
+            data=mpg, kind="line",
+            ci="sd")
+
+
+# Show plot
+plt.show()
+
+
+
+
+#Use sns.catplot() to create a count plot using the survey_data DataFrame with "Internet usage" on the x-axis.
+# Create count plot of internet usage
+sns.catplot(x="Internet usage", data=survey_data,
+            kind="count")
+
+# Show plot
+plt.show()
+
+
+
+#Make the bars horizontal instead of vertical.
+# Change the orientation of the plot
+sns.catplot(y="Internet usage", data=survey_data,
+            kind="count")
+
+# Show plot
+plt.show()
+
+
+
+#Create column subplots based on "Age Category", which separates respondents into those that are younger than 21 vs. 21 and older.
+# Create column subplots based on age category
+sns.catplot(y="Internet usage", data=survey_data,
+            kind="count", col="Age Category")
+
+# Show plot
+plt.show()
+
+
+
+
+
+#Use the survey_data DataFrame and sns.catplot() to create a bar plot with "Gender" on the x-axis and "Interested in Math" on the y-axis.
+# Create a bar plot of interest in math, separated by gender
+sns.catplot(x="Gender", y="Interested in Math",
+            data=survey_data, kind="bar")
+
+# Show plot
+plt.show()
+
+
+
+
+
+#Use sns.catplot() to create a bar plot with "study_time" on the x-axis and final grade ("G3") on the y-axis, using the student_data DataFrame.
+# Create bar plot of average final grade in each study category
+sns.catplot(x="study_time", y="G3",
+            data=student_data,
+            kind="bar")
+
+# Show plot
+plt.show()
+
+
+
+
+#Using the order parameter, rearrange the categories so that they are in order from lowest study time to highest.
+# Rearrange the categories
+sns.catplot(x="study_time", y="G3",
+            data=student_data,
+            kind="bar",
+            order=["<2 hours",
+                   "2 to 5 hours",
+                   "5 to 10 hours",
+                   ">10 hours"])
+
+# Show plot
+plt.show()
+
+
+
+
+#Update the plot so that it no longer displays confidence intervals.
+# Turn off the confidence intervals
+sns.catplot(x="study_time", y="G3",
+            data=student_data,
+            kind="bar",
+            order=["<2 hours",
+                   "2 to 5 hours",
+                   "5 to 10 hours",
+                   ">10 hours"],
+            ci=None)
+
+# Show plot
+plt.show()
+
+
+
+
+#Use sns.catplot() and the student_data DataFrame to create a box plot with "study_time" on the x-axis and "G3" on the y-axis. Set the ordering of the categories to study_time_order.
+# Specify the category ordering
+study_time_order = ["<2 hours", "2 to 5 hours",
+                    "5 to 10 hours", ">10 hours"]
+
+# Create a box plot and set the order of the categories
+sns.catplot(x="study_time", y="G3",
+            data=student_data,
+            kind="box",
+            order=study_time_order)
+
+# Show plot
+plt.show()
+
+
+
+#Use sns.catplot() to create a box plot with the student_data DataFrame, putting "internet" on the x-axis and "G3" on the y-axis.
+#Add subgroups so each box plot is colored based on "location".
+#Do not display the outliers.
+# Create a box plot with subgroups and omit the outliers
+sns.catplot(x="internet", y="G3",
+            data=student_data,
+            kind="box",
+            hue="location",
+            sym="")
+
+# Show plot
+plt.show()
+
+
+
+
+#Adjust the code to make the box plot whiskers to extend to 0.5 * IQR. Recall: the IQR is the interquartile range.
+# Set the whiskers to 0.5 * IQR
+sns.catplot(x="romantic", y="G3",
+            data=student_data,
+            kind="box",
+            whis=0.5)
+
+# Show plot
+plt.show()
+
+
+
+
+#Change the code to set the whiskers to extend to the 5th and 95th percentiles.
+# Extend the whiskers to the 5th and 95th percentile
+sns.catplot(x="romantic", y="G3",
+            data=student_data,
+            kind="box",
+            whis=[5, 95])
+
+# Show plot
+plt.show()
+
+
+
+
+
+#Change the code to set the whiskers to extend to the min and max values.
+# Set the whiskers at the min and max values
+sns.catplot(x="romantic", y="G3",
+            data=student_data,
+            kind="box",
+            whis=[0, 100])
+
+# Show plot
+plt.show()
+
+
+
+
+#Use sns.catplot() and the student_data DataFrame to create a point plot with "famrel" on the x-axis and number of absences ("absences") on the y-axis.
+# Create a point plot of family relationship vs. absences
+sns.catplot(x="famrel", y="absences",
+            data=student_data,
+            kind="point")
+
+# Show plot
+plt.show()
+
+
+
+#Add "caps" to the end of the confidence intervals with size 0.2.
+# Add caps to the confidence interval
+sns.catplot(x="famrel", y="absences",
+            data=student_data,
+            kind="point",
+            capsize=0.2)
+
+# Show plot
+plt.show()
+
+
+
+
+#Remove the lines joining the points in each category.
+# Remove the lines joining the points
+sns.catplot(x="famrel", y="absences",
+            data=student_data,
+            kind="point",
+            capsize=0.2,
+            join=False)
+
+# Show plot
+plt.show()
+
+
+
+
+#Use sns.catplot() and the student_data DataFrame to create a point plot with relationship status ("romantic") on the x-axis and number of absences ("absences") on the y-axis. Create subgroups based on the school that they attend ("school")
+# Create a point plot with subgroups
+sns.catplot(x="romantic", y="absences",
+			data=student_data,
+            kind="point",
+            hue="school")
+
+# Show plot
+plt.show()
+
+
+
+
+#Turn off the confidence intervals for the plot.
+# Turn off the confidence intervals for this plot
+sns.catplot(x="romantic", y="absences",
+			data=student_data,
+            kind="point",
+            hue="school",
+            ci=None)
+
+# Show plot
+plt.show()
+
+
+
+
+#Since there may be outliers of students with many absences, import the median function from numpy and display the median number of absences instead of the average.
+# Import median function from numpy
+from numpy import median
+
+# Plot the median number of absences instead of the mean
+sns.catplot(x="romantic", y="absences",
+			data=student_data,
+            kind="point",
+            hue="school",
+            ci=None,
+            estimator=median)
+
+# Show plot
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
